09adda180ee1d22da79ff63f2d0e22f972c239a1
Mr.Dokepon <Gigabiz.it@gmail.com>
2017-02-02 14:38:49 +0700

    fix import form-address

--- test/watch.js
+++ test/watch.js
@@ -2,18 +2,18 @@ import React from 'react'
 import { expect } from 'chai'
 import { mount, shallow } from 'enzyme'
 
-import Input from '../components/input'
+import FormAddress from '../components/form-address'
 
 describe('Balance', () => {
     describe('Input from', () => {
 
         it('should have input address', () => {
-            const wrapper = shallow(<Input />);
+            const wrapper = shallow(<FormAddress />);
             expect(wrapper.find('input')).to.have.length(1);
         })
 
         it('should have submit button', () => {
-            const wrapper = shallow(<Input />);
+            const wrapper = shallow(<FormAddress />);
             let button = wrapper.find('[type="submit"]').get(0);
             expect(button.props.children).to.equal('Get Balance');
         })
